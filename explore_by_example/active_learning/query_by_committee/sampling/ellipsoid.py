added numerically stable implementation to rounding; fixed hyperplane cutting in rounding

explore_by_example/active_learning/query_by_committee/sampling/ellipsoid.py
@@ -7,19 +7,27 @@ class Ellipsoid:
     where P is SPD and z is the center.
     """
 
-    def __init__(self, x, P):
+    def __init__(self, dim):
         """
         Inputs:
             center: ellipsoid's center x
             matrix: SPD matrix P^-1
         """
-        self.x = np.atleast_1d(x).ravel()
-        self.P = np.atleast_2d(P)
-        self.n = len(self.x)
+        if dim <= 0:
+            raise ValueError("dim must be a positive integer, but received " + dim)
+
+        self.x = np.zeros(dim)
+        self.n = dim
+
+        self.L = np.eye(self.n)
+        self.D = np.ones(self.n)
+        self.P = np.eye(dim)
+
         assert self.P.shape == (self.n, self.n)
 
     def __repr__(self):
         return str(self.x) + "\n" + str(self.P)
+
 
     def get_endpoints(self, factor=1.0):
         eig, P = np.linalg.eigh(self.P)
@@ -28,15 +36,52 @@ class Ellipsoid:
             yield self.x + factor * a
             yield self.x - factor * a
 
-    def update(self, g):
-        Pg = self.P.dot(g)
-        Pg_norm = Pg / np.sqrt(g.dot(Pg))
-
+    def update(self, b, g):
         n = self.n
-        self.x = self.x - Pg_norm / (n + 1)
-        self.P = (n ** 2 / (n ** 2 - 1)) * (self.P - (2.0 / (n + 1)) * Pg_norm.reshape(-1, 1).dot(Pg_norm.reshape(1, -1)))
 
-    def weak_ellipsoid(self, pol):
+        a_hat = self.L.T.dot(g)  # a_hat = L^T g
+        gamma = np.sqrt(np.square(a_hat).dot(self.D))  # gamma = sqrt(a_hat^T D a_hat)
+        alpha = (g.dot(self.x) - b) / gamma
+
+        p = self.D * a_hat / gamma
+        Pg = self.L.dot(p)
+        # update center
+        tau = (1 + n * alpha) / (n + 1)
+        self.x -= tau * Pg
+
+        # update LDL^T decomposition
+        sigma = 2 * tau / (alpha + 1)
+        delta = (1 - alpha * alpha) * (n * n / (n * n - 1.))
+        beta = self.update_diagonal(p, sigma, delta)
+        self.update_cholesky_factor(p, beta)
+
+        # update P
+        self.P -= sigma * Pg.reshape(-1, 1).dot(Pg.reshape(1, -1))
+        self.P *= delta
+        assert np.allclose(self.P, self.L.dot(np.diag(self.D)).dot(self.L.T), atol=1e-15)
+
+    def update_diagonal(self, p, sigma, delta):
+        """ LDL^T for D - sigma pp^T """
+        t = np.empty(len(self.D) + 1)
+        t[-1] = 1 - sigma * p.dot(p / self.D)
+        t[:-1] = sigma * np.square(p) / self.D
+        t = np.cumsum(t[::-1])[::-1]
+
+        self.D *= t[1:]
+        beta = -sigma * p / self.D
+        self.D /= t[:-1]
+        self.D *= delta
+
+        return beta
+
+    def update_cholesky_factor(self, p, b):
+        """ Computes the product L K, with L and K lower triangular with unit diagonal, and Kij = pi * bj for i > j """
+        v = self.L * p.reshape(1, -1)
+        v = np.cumsum(v[:, ::-1], axis=1)[:, ::-1]
+
+        self.L[:, :-1] += v[:, 1:] * b[:-1].reshape(1, -1)
+
+    def fit(self, pol):
         converge = False
         while not converge:
             converge = True
@@ -44,11 +89,13 @@ class Ellipsoid:
             bad_constrain = pol.get_separating_oracle(self.x)
             if bad_constrain is not None:
                 converge = False
-                self.update(bad_constrain)
+                b, g = bad_constrain
+                self.update(b, g)
             else:
                 for end_point in self.get_endpoints(factor=1.0 / (self.n + 1)):
                     bad_constrain = pol.get_separating_oracle(end_point)
                     if bad_constrain is not None:
                         converge = False
-                        self.update(bad_constrain)
+                        b, g = bad_constrain
+                        self.update(b,g)
                         break

explore_by_example/active_learning/query_by_committee/sampling/hit_and_run.py
@@ -103,12 +103,12 @@ class LinearVersionSpace:
         :param point: data point
         :return: normal vector to separating hyperplane. If no such plane exists, returns None
         """
+        if np.dot(point, point) > 1:
+            return 1, point / np.linalg.norm(point)
+
         for a in self.A:
             if np.dot(a, point) > 0:
-                return a
-
-        if np.dot(point, point) > 1:
-            return point
+                return 0, a
 
         return None
 
@@ -167,9 +167,9 @@ class HitAndRunSampler:
 
         # rounding
         if self.rounding:
-            elp = Ellipsoid(x=np.zeros(dim), P=np.eye(dim))
-            elp.weak_ellipsoid(version_space)
-            rounding_matrix = np.linalg.cholesky(elp.P)
+            elp = Ellipsoid(dim)
+            elp.fit(version_space)
+            rounding_matrix = elp.L * np.sqrt(elp.D).reshape(1, -1)
 
         samples = []
         for i in range(self.warmup + self.thin * n_samples):