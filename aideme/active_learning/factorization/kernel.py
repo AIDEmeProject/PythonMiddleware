Fixed bug in kernel.py: wrong kernel matrix shape Fixed bug in optimization.py: wrong number of iterations and batch-gradient shuffling was not synchronized with run start

aideme/active_learning/factorization/kernel.py
@@ -88,7 +88,7 @@ class KernelFactorizationLearner:
             else:
                 x0 = x0 @ K
 
-        self.fact_linear.fit(K, y, factorization, retries, x0)
+        res = self.fact_linear.fit(K, y, factorization, retries, x0)
 
         if self._use_cholesky:
             if self._factorization is None:
@@ -96,6 +96,8 @@ class KernelFactorizationLearner:
             else:
                 for k in range(K.shape[2]):
                     self.fact_linear._weights[k] = scipy.linalg.solve_triangular(K[:, :, k], self.fact_linear._weights[k], lower=True, trans=1)
+
+        return res
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         return self.fact_linear.predict(self._get_kernel_matrix(X))
@@ -110,7 +112,7 @@ class KernelFactorizationLearner:
         if self._factorization is None:
             return self.__kernel_matrix_helper(X, cholesky)
 
-        K = np.empty((X.shape[0], X.shape[0], len(self._factorization)))
+        K = np.empty((X.shape[0], self._X_train.shape[0], len(self._factorization)))
         for k, p in enumerate(self._factorization):
             K[:, :, k] = self.__kernel_matrix_helper(X, cholesky, p)
         return K

aideme/active_learning/factorization/optimization.py
@@ -51,7 +51,7 @@ class OptimizationAlgorithm:
         new_x = x0.copy()
         self.__process_new_iter(grad, new_x, result)
 
-        while not result.success and result.it <= self._max_iter:
+        while not result.success and result.it < self._max_iter:
             new_x = self._advance(result, func, grad)
             self.__process_new_iter(grad, new_x, result)
 
@@ -75,17 +75,16 @@ class OptimizationAlgorithm:
         result.success = self.__converged(result)
         self.__run_callback(result)
 
-    @staticmethod
-    def __update_result_object(result: OptimizeResult, new_x: np.ndarray, grad: Callable):
+    def __update_result_object(self, result: OptimizeResult, new_x: np.ndarray, grad: Callable):
         result.it += 1
         result.prev, result.x = result.x, new_x
-        result.grad = grad(new_x)
+        result.grad = grad(new_x) if result.it < self._max_iter else None
 
     def __converged(self, result: OptimizeResult) -> bool:
         if result.prev is not None and np.linalg.norm(result.x - result.prev) < self._rel_tol * np.linalg.norm(result.x):
             return True
 
-        return self._gradient_converged(result, self._gtol)
+        return self._gradient_converged(result, self._gtol) if result.grad is not None else False
 
     def __run_callback(self, result: OptimizeResult) -> None:
         if self._callback is not None: