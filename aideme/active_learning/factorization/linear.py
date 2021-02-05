Fixed numerical instability in gradient computation (LinearFactorizationLoss) Use in-built power function in Adam for better accuracy Removed unused function in utils.pyx

aideme/active_learning/factorization/linear.py
@@ -247,7 +247,7 @@ class LinearFactorizationLoss:
             X, y = self.X[idx], self.y[idx]
 
         margins = self.__compute_margin(X, bias, weights)
-        grad_weights = utils.compute_grad_factors(margins, y)
+        grad_weights = self.__compute_grad_weights(margins, y)
         grad_b, grad_w = self.__compute_grad(X, grad_weights)
 
         # add penalty terms
@@ -284,6 +284,16 @@ class LinearFactorizationLoss:
             grad_b = grad_weights.sum(axis=0)
 
         return grad_b, grad_w
+
+    def __compute_grad_weights(self, margins: np.ndarray, y: np.ndarray):
+        probas = expit(margins)
+
+        weights = probas.prod(axis=1)
+        np.true_divide(weights, 1 - weights, out=weights, where=weights < 1)
+        weights[y > 0] = -1
+        weights /= margins.shape[0]
+
+        return (1 - probas) * weights.reshape(-1, 1)
 
     def get_weights_matrix(self, weights: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         if self.factorization is None:

aideme/active_learning/factorization/optimization.py
@@ -190,23 +190,20 @@ class Adam(SearchDirectionOptimizer):
         self._beta2 = beta2
         self._epsilon = epsilon
 
-        self._beta1_t = 1.0
-        self._beta2_t = 1.0
         self._mt = self._vt = 0
 
     def _reset(self):
-        self._beta1_t = 1.0
-        self._beta2_t = 1.0
         self._mt = self._vt = 0
 
     def _compute_search_dir(self, result: OptimizeResult) -> np.ndarray:
         self._mt = self._beta1 * self._mt + (1 - self._beta1) * result.grad
         self._vt = self._beta2 * self._vt + (1 - self._beta2) * np.square(result.grad)
 
-        self._beta1_t *= self._beta1
-        self._beta2_t *= self._beta2
+        it = result.it + 1
+        beta1_t = self._beta1 ** it
+        beta2_t = self._beta2 ** it
 
-        m_hat = self._mt / (1 - self._beta1_t)
-        v_hat = self._vt / (1 - self._beta2_t)
+        m_hat = self._mt / (1 - beta1_t)
+        v_hat = self._vt / (1 - beta2_t)
 
         return m_hat / (np.sqrt(v_hat) + self._epsilon)